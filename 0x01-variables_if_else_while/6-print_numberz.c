c9866ad4beafe0f490394cdc8bbcf79df298bb6e
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-03-17 18:37:48 -0700

    create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0 ; b < 10 ; b++)
+	{
+		putchar(b + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
