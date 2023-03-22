4c3f8d85c674493ba477600d9ad7df110f4076ec
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-03-21 17:55:14 -0700

    created function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - print the sum of two numbers
+ *@a: first parameter of the function
+ *@b: second parameter
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
