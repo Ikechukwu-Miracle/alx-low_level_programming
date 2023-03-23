aacc8d2c65dc0439d0d09fe24d91210fda9ac57d
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-03-23 13:08:26 -0700

    created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - gives the multiple of two numbers
+ * @a: parameter of the function
+ * @b: second parameter
+ * Return: value of mul1
+ */
+
+int mul(int a, int b)
+{
+	int mul1;
+
+	mul1 = a * b;
+
+	return (mul1);
+}
