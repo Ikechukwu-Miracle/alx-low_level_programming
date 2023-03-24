83213794c9050b1ad631311b404b20349db296b9
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-03-23 17:07:20 -0700

    created 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonals
+ * @n: parameter of the function
+ * Return: always 0
+ */
+
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 0 ; c < n ; c++)
+		{
+			for (d = 0 ; d < c ; d++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
