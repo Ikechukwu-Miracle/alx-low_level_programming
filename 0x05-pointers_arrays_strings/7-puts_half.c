308a6ba397580e159f4b5a2dedf77463cf6fdcb7
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-03-27 15:17:54 -0700

    created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2 ; i <= length ; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (length - 1) / 2 ; i <= length ; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
