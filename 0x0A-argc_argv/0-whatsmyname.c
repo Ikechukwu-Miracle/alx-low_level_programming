91d7e90e4e689dd14eade2fb73b8f72d95316cf9
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-06 03:50:33 -0700

    created 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ * @argc: counts number of arguments
+ * @argv: pointer to arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,8 @@
+**0x0A. C - argc, argv**
+
+This project is on the argc and argv arguments to main
+There are a total of 6 tasks in the project:
+5 mandatory and 1 advanced.
+
+*0. It ain't what they call you, it's what you answer to* is a program that prints its name.
+
