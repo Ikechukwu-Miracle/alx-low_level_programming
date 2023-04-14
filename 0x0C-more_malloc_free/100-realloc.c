4cfef9921169b1625afd56a5d962719407fa382c
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-14 09:34:06 -0700

    edited 101-mul.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,5 +43,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	free(ptr);
 
-	return (pt);
+return (pt);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "main.h"
 #define ERR_MSG "Error"
 
 /**
