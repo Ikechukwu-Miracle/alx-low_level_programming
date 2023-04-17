2cfbbb36b576ecf273deabb3547f4eb2fc3d7322
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-17 12:03:08 -0700

    defined a new type for stuct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,15 +17,14 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
