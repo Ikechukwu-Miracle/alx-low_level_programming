05801e5d5e8496ddfa1e399f05a147a69218da8a
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-17 10:35:02 -0700

    created dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+**0x0E. C - Structures, typedef**
+
+This project is on C - Structures and Typedef.
+There are a total of 6 mandatory tasks in the peoject:
+
+*0. Poppy* defines a new type struct dog.
+
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains information about dogs
+ * @name: this the name of the dog
+ * @age: the age of the dog
+ * @owner: who owns the dog
+ *
+ * Description: The struct dog holds variables of different dogs
+ * and thier owners
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
