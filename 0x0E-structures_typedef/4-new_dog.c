d12f4219b2b213970e7c92b0d6a6ed93df5896b0
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-17 12:54:39 -0700

    created 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,10 +25,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, length;
 
-	length = _strlen(src);
+	length = 0;
+	while (src[length] != '\0')
+		length++;
 
 	for (i = 0 ; i < length ; i++)
 		dest[i] = src[i];
+	dest[i] = '\0';
 
 	return (dest);
 }
