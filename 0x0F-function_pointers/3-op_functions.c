cbdb66c0e3281c092b496b81fc26819f92ad1c15
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-19 16:50:41 -0700

    created program files for arithmetics

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two nombers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts second number
+ * from first number
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - gets the integer value of divided number
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder of the division of a and b
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -6,5 +6,6 @@ There are a total of 5 tasks in this project: 4 mandatory and 1 advanced.
 *0. What's my name* is a function that prints a name.
 *1. If you spend too much time thinking about a thing, you'll never get it done* is a function that executes a function on every element of an array.
 *2. To hell with circumstances; I create opportunities* is a function that searches for an integer in an array.
+*3. A goal is not always meant to be reached, it often serves simply as something to aim at* is a program that performs arithmetic operations to arguments passed to it.
 
 
