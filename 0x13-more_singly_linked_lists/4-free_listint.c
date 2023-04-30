aa3185d7ffd6ea57f05ca291419392cafc1dc698
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-30 09:52:55 -0700

    created 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees listint_t
+ * @head: pointer to linked list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
