3c8832d58e95d42cd7c27dbc4b009be64882f0e9
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-30 16:20:12 -0700

    created 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,8 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *tmp = head;
 
-	for (i = 0 ; i < index ; i++)
+	while (i < index)
+	{
 		tmp = tmp->next;
+		i++;
+	}
 
 	if (tmp->next == NULL)
 		return (NULL);
