76403a46034be5ac2917dc1da60d3c9b24dd876c
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-05-01 04:42:47 -0700

    created 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = currNode->next;
 		free(currNode);
+		return (1);
 	}
 	else
 	{
@@ -29,7 +30,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		prevNode->next = currNode->next;
 		free(currNode);
+		return (1);
 	}
-
-	return (1);
 }
