309b8c5aa46ec4027640311ced59f01e147138cd
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-05-01 03:47:45 -0700

    another fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,16 +25,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		newNode->next = *head;
 		*head = newNode;
+		return (newNode);
 	}
 
-	for (i = 0 ; i < idx - 1 ; i++)
+	for (i = 0 ; tmp && i < idx ; i++)
+	{
+		if (i == idx - 1)
+		{
+			newNode->next = tmp->next;
+			tmp->next = newNode;
+			return (newNode);
+		}
 		tmp = tmp->next;
-
-	if (!tmp)
-		return (NULL);
-
-	newNode->next = tmp->next;
-	tmp->next = newNode;
-
-	return (newNode);
+	}
+	return (NULL);
 }
