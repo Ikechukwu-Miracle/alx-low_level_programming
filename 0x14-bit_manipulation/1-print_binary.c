5eeca61fe6b6eae623044d8ef606b1ea902530b6
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-07-07 22:03:59 +0000

    created 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent
+ * of a decimal value pass to it
+ * @n: the decimal number
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int currNum;
+	int x, bin_count = 0;
+
+	x = 63;
+	while (x >= 0)
+	{
+		currNum = n >> x;
+		if (currNum & 1)
+		{
+			_putchar('1');
+			bin_count++;
+		}
+		else if (bin_count)
+			_putchar('0');
+		x--;
+	}
+	if (!bin_count)
+		_putchar('0');
+}
