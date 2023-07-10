306de0b7bd46d2bcdc3c1bc4bad0c639867e30af
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-07-10 20:18:50 +0000

    modified 1-create_textfile.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,22 +14,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		length = 0;
-		while (text_content[length] != '\0')
-			length++;
-	}
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	written = write(fd, text_content, length);
-
-	if (written == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		for (length = 0 ; text_content[length] ;)
+			length++;
+		written = write(fd, text_content, length);
+		if (written == -1)
+			return (-1);
+	}
 
 	close(fd);
 	return (1);
