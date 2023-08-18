40f8ac36f45cba49538b0e66834840dcb443cd29
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-08-18 20:58:16 +0000

    created 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets a node at n position
+ * @head: pointer to the first element of the list
+ * @index: position of the specific node 
+ *
+ * Return: pointer to node at position index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int n = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (temp)
+	{
+		if (index == n)
+			return (temp);
+		n++;
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
