54147bf9931f3d518a0bef3bebbd01159bd25241
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-09-09 02:39:49 +0000

    created 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gets the index at which the kay/value
+ * should be stored in the given array
+ * @key: the key of the hash table to get index for
+ *
+ * Return: the require index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int keyIndex = hash_djb2(key) % size;
+
+	return (keyIndex);
+}
